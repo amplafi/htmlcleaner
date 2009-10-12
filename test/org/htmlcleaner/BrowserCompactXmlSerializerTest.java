24265678dd35c4b9010f284e824382cce7bf0c3c
Konstantin Burov <aectann@gmail.com>
2009-10-12 11:45:04 +0000

    White space clearing test.
    git-svn-id: https://svn1.hosted-projects.com/transparentpolitics/modified-opensource/htmlcleaner/trunk@105 9eb8ad66-390e-4894-8ab5-5ca26f751fd9

---
+++ test/org/htmlcleaner/BrowserCompactXmlSerializerTest.java
@@ -0,0 +1,28 @@
+package org.htmlcleaner;
+
+import junit.framework.TestCase;
+
+/**
+ * Test cases for for {@link BrowserCompactXmlSerializer}
+ * 
+ * @author Konstantin Burov (aectann@gmail.com)
+ *
+ */
+public class BrowserCompactXmlSerializerTest extends TestCase {
+
+    /**
+     * Tests that serializer removes white spaces properly.
+     */
+    public void testRemoveInsignificantWhitespaces(){
+        CleanerProperties properties = new CleanerProperties();
+        properties.setOmitHtmlEnvelope(true);
+        properties.setOmitXmlDeclaration(true);
+        BrowserCompactXmlSerializer compactXmlSerializer = new BrowserCompactXmlSerializer(properties);
+        String cleaned = compactXmlSerializer.getXmlAsString(properties, "<u>text here,</u>    <b>some       text</b>", "UTF-8");
+        assertEquals("<u>text here,</u> <b>some text</b>", cleaned);
+        cleaned = compactXmlSerializer.getXmlAsString(properties, "<p><font color=\"#ff0000\" size=\"1\">First test reply</font></p><p>  </p><hr/><p></p>", "UTF-8");
+        assertEquals("<p><font color=\"#ff0000\" size=\"1\">First test reply</font></p><p> </p><hr /><p />", cleaned);
+        cleaned = compactXmlSerializer.getXmlAsString(properties, "<div class=\"foo\">2 roots < here ></div>", "UTF-8");
+        assertEquals("<div class=\"foo\">2 roots &lt; here &gt;</div>", cleaned);
+    }
+}
