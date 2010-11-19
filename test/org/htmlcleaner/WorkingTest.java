2c8cff1bd6c90ffbdd4ab7df3eedb455a82eb829
vnikic <vnikic@482c6979-392e-0410-9d6c-a8d33d214f2b>
2010-11-19 09:38:39 +0000

    Ant xml fixes
    git-svn-id: svn+ssh://svn.code.sf.net/p/htmlcleaner/code/trunk@118 482c6979-392e-0410-9d6c-a8d33d214f2b

--- test/org/htmlcleaner/WorkingTest.java
+++ test/org/htmlcleaner/WorkingTest.java
@@ -34,50 +34,50 @@ public class WorkingTest {
 //        props.setBooleanAttributeValues("empty");
 //        props.setNamespacesAware(false);
 
-//        final String resources[] = {
-//                "http://www.b92.net",
-//                "http://www.nba.com",
-//                "http://www.naslovi.net/",
-//                "http://www.theserverside.com/",
-//                "http://www.yahoo.com",
-//        };
         final String resources[] = {
-                "c:/temp/htmlcleanertest/1.htm",
-                "c:/temp/htmlcleanertest/2.htm",
-                "c:/temp/htmlcleanertest/3.htm",
-                "c:/temp/htmlcleanertest/4.htm",
-                "c:/temp/htmlcleanertest/5.htm",
+                "http://www.b92.net",
+                "http://www.nba.com",
+                "http://www.naslovi.net/",
+                "http://www.theserverside.com/",
+                "http://www.yahoo.com",
         };
+//        final String resources[] = {
+//                "c:/temp/htmlcleanertest/1.htm",
+//                "c:/temp/htmlcleanertest/2.htm",
+//                "c:/temp/htmlcleanertest/3.htm",
+//                "c:/temp/htmlcleanertest/4.htm",
+//                "c:/temp/htmlcleanertest/5.htm",
+//        };
 
         final PrettyXmlSerializer prettySerializer = new PrettyXmlSerializer(props);
 
         long start = System.currentTimeMillis();
 
-//        for (int i = 0; i < resources.length; i++) {
-//            TagNode node = cleaner.clean(new File(resources[i]));
-//            prettySerializer.writeXmlToFile(node, "c:/temp/htmlcleanertest/out/" + i + ".xml", "UTF-8");
-//        }
-//        System.out.println("Vreme u jednom tredu: " + (System.currentTimeMillis() - start));
-//
-//        final long start1 = System.currentTimeMillis();
-//
-//        for (int i = 0; i < resources.length; i++) {
-//            final int index = i;
-//            new Thread(new Runnable() {
-//                public void run() {
-//                    try {
-//                        TagNode node = cleaner.clean(new File(resources[index]));
-//                        prettySerializer.writeXmlToFile(node, "c:/temp/htmlcleanertest/out/" + index + "a.xml", "UTF-8");
-//                        System.out.println("Vreme u tredu " + index + ": " + (System.currentTimeMillis() - start1));
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            }).start();
-//        }
+        for (int i = 0; i < resources.length; i++) {
+            TagNode node = cleaner.clean(new URL(resources[i]));
+            prettySerializer.writeXmlToFile(node, "c:/temp/htmlcleanertest/out/" + i + ".xml", "UTF-8");
+        }
+        System.out.println("Vreme u jednom tredu: " + (System.currentTimeMillis() - start));
 
-        TagNode node = cleaner.clean(new File("c:/temp/test.html"));
-        prettySerializer.writeXmlToFile(node, "c:/temp/htmlcleanertest/1.xml", "utf-8");
+        final long start1 = System.currentTimeMillis();
+
+        for (int i = 0; i < resources.length; i++) {
+            final int index = i;
+            new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        TagNode node = cleaner.clean(new URL(resources[index]));
+                        prettySerializer.writeXmlToFile(node, "c:/temp/htmlcleanertest/out/" + index + "a.xml", "UTF-8");
+                        System.out.println("Vreme u tredu " + index + ": " + (System.currentTimeMillis() - start1));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+
+//        TagNode node = cleaner.clean(new File("c:/temp/test.html"));
+//        prettySerializer.writeXmlToFile(node, "c:/temp/htmlcleanertest/1.xml", "utf-8");
     }
 
 }
