029b04e92652c4050955953447b496363f60ba89
eg <blackorangebox@gmail.com>
2013-02-23 19:35:50 +0700

    added test testEscapeXml_recognizeUnicodeChars()

--- src/test/java/org/htmlcleaner/UtilsTest.java
+++ src/test/java/org/htmlcleaner/UtilsTest.java
@@ -16,4 +16,12 @@ public class UtilsTest extends TestCase {
         res = Utils.escapeXml("2.&amp;&quot;&apos;&lt;&gt;", true, true, true, false, false);
         assertEquals("2.&amp;&quot;&apos;&lt;&gt;", res);
     }
+    
+    public void testEscapeXml_recognizeUnicodeChars() {
+        String res = Utils.escapeXml("[&alpha;][&eacute;][&oline;]", true, false, true, false, false);
+        assertEquals("[&#945;][&#233;][&#8254;]", res);
+        
+        res = Utils.escapeXml("[&alpha;][&eacute;][&oline;][&#931;]", true, true, true, false, false);
+        assertEquals("[α][é][‾][Σ]", res);
+    }
 }
