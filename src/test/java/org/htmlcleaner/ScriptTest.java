29e51f3d2b6dff434b9a216b976171011f3d82d0
scottwilson <scottwilson@482c6979-392e-0410-9d6c-a8d33d214f2b>
2013-02-26 19:56:10 +0000

    Tests some common <script> tag usage; currently also shows up an issue with TagNode.getChildren() returning ContentNode instances.
    git-svn-id: svn+ssh://svn.code.sf.net/p/htmlcleaner/code/trunk@205 482c6979-392e-0410-9d6c-a8d33d214f2b

---
+++ src/test/java/org/htmlcleaner/ScriptTest.java
@@ -0,0 +1,46 @@
+package org.htmlcleaner;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Tests for some common use of <script> tags within <head> elements
+ * @author scottw
+ *
+ */
+public class ScriptTest {
+	
+	@Test
+	public void getScripts() throws IOException{
+	    HtmlCleaner cleaner = new HtmlCleaner();
+        TagNode html = cleaner.clean( new File("src/test/resources/script_test.html") );
+        TagNode head = html.findElementByName("head", false);
+        
+        ArrayList<TagNode> scripts = new ArrayList<TagNode>();
+		@SuppressWarnings("unchecked")
+		
+		// 
+		// This test currently fails, replace with head.getChildTagList and it works
+		//
+		List<TagNode> children = head.getChildren();	
+		
+		for(TagNode child : children){						
+			if(child.getName().equals("script")){				
+				scripts.add(child);
+			}			
+		}
+		assertEquals(3, scripts.size());
+		assertEquals("x.js", scripts.get(0).getAttributeByName("src"));
+		assertEquals("y.js", scripts.get(1).getAttributeByName("src"));
+		assertEquals("z.js", scripts.get(2).getAttributeByName("src"));
+
+	}
+
+}
