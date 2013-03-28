d46d8dedc5e7a9afe2a7dafbc86bbd65237375b9
patmoore <patmoore@482c6979-392e-0410-9d6c-a8d33d214f2b>
2013-03-28 03:51:35 +0000

    additional tests as reported in bug database
    git-svn-id: svn+ssh://svn.code.sf.net/p/htmlcleaner/code/trunk@222 482c6979-392e-0410-9d6c-a8d33d214f2b

--- src/test/java/org/htmlcleaner/HtmlCleanerTest.java
+++ src/test/java/org/htmlcleaner/HtmlCleanerTest.java
@@ -7,10 +7,10 @@ import java.io.StringWriter;
 import junit.framework.TestCase;
 
 public class HtmlCleanerTest extends TestCase {
-	
+
 	private HtmlCleaner cleaner;
 	private XmlSerializer serializer;
-	
+
     @Override
     protected void setUp() throws Exception {
         CleanerProperties cleanerProperties = new CleanerProperties();
@@ -19,21 +19,21 @@ public class HtmlCleanerTest extends TestCase {
         cleanerProperties.setAdvancedXmlEscape(true);
         cleanerProperties.setTranslateSpecialEntities(false);
         cleanerProperties.setOmitComments(false);
-        
+
         cleaner = new HtmlCleaner(cleanerProperties);
         serializer = new SimpleXmlSerializer(cleanerProperties);
     }
-    
-    /** 
-     * Test for #2901.  
+
+    /**
+     * Test for #2901.
      */
 	public void testWhitespaceInHead() throws IOException {
 		String initial = readFile("src/test/resources/Real_1.html");
 		String expected = readFile("src/test/resources/Expected_1.html");
 		assertCleaned(initial, expected);
 	}
-	
-	/** 
+
+	/**
 	 * Mentioned in #2901 - we should eliminate the first <tr>
 	 * TODO: Passes but not with ideal result.
 	 */
@@ -41,23 +41,23 @@ public class HtmlCleanerTest extends TestCase {
 		cleaner.getProperties().setAddNewlineToHeadAndBody(false);
 		String start = "<html><head /><body><table>";
 		String end = "</body></html>";
-		assertCleaned(start + "<tr><tr><td>stuff</td></tr>" + end, 
+		assertCleaned(start + "<tr><tr><td>stuff</td></tr>" + end,
 				//start+"<tbody><tr><td>stuff</td></tr></tbody></table>" + end // "ideal" output
 				start + "<tbody><tr /><tr><td>stuff</td></tr><tr></tr></tbody></table>" + end // actual
 		);
 	}
-	
-	/** 
+
+	/**
 	 * Collapsing empty tr to <tr />
 	 */
 	public void testUselessTr2() throws IOException {
 		cleaner.getProperties().setAddNewlineToHeadAndBody(false);
 		String start = "<html><head /><body><table>";
 		String end = "</table></body></html>";
-		assertCleaned(start + "<tr> </tr><tr><td>stuff</td></tr>" + end, 
+		assertCleaned(start + "<tr> </tr><tr><td>stuff</td></tr>" + end,
 				start + "<tbody><tr /><tr><td>stuff</td></tr></tbody>" + end);
 	}
-	
+
 	/**
 	 * For #2940
 	 */
@@ -65,10 +65,10 @@ public class HtmlCleanerTest extends TestCase {
 		cleaner.getProperties().setAddNewlineToHeadAndBody(false);
 		String start = "<html><head>";
 		String end = "</head><body>1</body></html>";
-		assertCleaned(start + "<style type=\"text/css\">/*<![CDATA[*/\n#ampmep_188 { }\n/*]]>*/</style>" + end, 
+		assertCleaned(start + "<style type=\"text/css\">/*<![CDATA[*/\n#ampmep_188 { }\n/*]]>*/</style>" + end,
 				start + "<style type=\"text/css\">/*<![CDATA[*/\n#ampmep_188 { }\n/*]]>*/</style>" + end);
-	}	
-	
+	}
+
 	/**
 	 * Report in issue #64 as causing issues.
 	 * @throws Exception
@@ -79,16 +79,32 @@ public class HtmlCleanerTest extends TestCase {
 	    final TagNode[] imgNodes = node.getElementsByName("img", true);
 	    assertEquals(5, imgNodes.length);
 	}
+    /**
+     * Report in issue #70 as causing issues.
+     * @throws Exception
+     */
+    public void testOOME_70() throws Exception {
+        String initial = readFile("src/test/resources/oome_70.html");
+        TagNode node = cleaner.clean(initial);
+        final TagNode[] imgNodes = node.getElementsByName("img", true);
+        assertEquals(17, imgNodes.length);
+    }
 
+    public void testOOME_59() throws Exception {
+        String in = "<html><body><table><fieldset><legend>";
+        CleanerProperties cp = new CleanerProperties();
+        HtmlCleaner c = new HtmlCleaner(cp);
+        TagNode root = c.clean(in);
+    }
 	private void assertCleaned(String initial, String expected) throws IOException {
         TagNode node = cleaner.clean(initial);
         StringWriter writer = new StringWriter();
         serializer.serialize(node, writer);
-        assertEquals(expected, writer.toString());		
+        assertEquals(expected, writer.toString());
 	}
-	
+
 	private String readFile(String filename) throws IOException {
-		File file = new File(filename);		
+		File file = new File(filename);
 		CharSequence content = Utils.readUrl(file.toURI().toURL(), "UTF-8");
 		return content.toString();
 	}
