743559215a5b9f347e873fdb0eec4668bdd8eeb4
scottwilson <scottwilson@482c6979-392e-0410-9d6c-a8d33d214f2b>
2013-05-23 18:52:19 +0000

    Removed debug code
    git-svn-id: svn+ssh://svn.code.sf.net/p/htmlcleaner/code/trunk@246 482c6979-392e-0410-9d6c-a8d33d214f2b

--- src/test/java/org/htmlcleaner/SerializationTest.java
+++ src/test/java/org/htmlcleaner/SerializationTest.java
@@ -4,19 +4,12 @@ import junit.framework.TestCase;
 
 import java.io.IOException;
 import java.io.File;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 
 import org.junit.Test;
 import org.w3c.dom.Document;
 
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 /**
  * Testing node manipulation after cleaning.
@@ -133,17 +126,6 @@ public class SerializationTest extends TestCase {
         String xml3 = new CompactXmlSerializer(properties).getAsString(node);
         assertTrue(xml3.indexOf("Moja mala nema mane...") >= 0);
 
-    }
-    
-    public static void printDocument(Document doc, OutputStream out) throws IOException, TransformerException {
-        TransformerFactory tf = TransformerFactory.newInstance();
-        Transformer transformer = tf.newTransformer();
-        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
-        transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-        transformer.transform(new DOMSource(doc), 
-             new StreamResult(new OutputStreamWriter(out, "UTF-8")));
     }
 
 }
